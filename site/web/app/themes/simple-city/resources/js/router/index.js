0a6b1054fda0e82957b3b98b0072f84c852b5952
Giannisduke <admin@eboy.gr>
2025-07-12 22:37:15 +0300

    new app.js

--- site/web/app/themes/simple-city/resources/js/router/index.js
+++ site/web/app/themes/simple-city/resources/js/router/index.js
@@ -1,17 +1,11 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Home from "../../views/Posts.vue";
-import About from "../../views/About.vue";
 
 const routes = [
   {
     path: "/",
     name: "Home",
     component: Home,
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: About,
   },
 ];
 
