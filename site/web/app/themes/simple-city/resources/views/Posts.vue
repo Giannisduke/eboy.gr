e5c6824c8c7ebd016598a61319ca74e933e66595
Giannisduke <admin@eboy.gr>
2025-09-09 13:23:20 +0300

    api url new

--- site/web/app/themes/simple-city/resources/views/Posts.vue
+++ site/web/app/themes/simple-city/resources/views/Posts.vue
@@ -133,7 +133,7 @@ import axios from "axios";
         // Pulls posts when the component is created.
         created() {
             axios
-                .get('https://eboy.gr/wp-json/wc/v3/products?featured=true&consumer_key=ck_1bfa47417a538aa339e45a95fcecffda38d1891d&consumer_secret=cs_9babca3e5c1bad1b420748e2ae464393de0d1995')
+                .get('https://eboy.gr/wp-json/wc/v3/products?featured=true&consumer_key=ck_5d8a95b30ef429cb8ef4c33c7a69e46a1e2ef3ae&consumer_secret=cs_45e0c75bcf6d54396fc3d896e180aa90300495af')
                 .then((response) => {
                     // JSON responses are automatically parsed.
                     this.products = response.data;
