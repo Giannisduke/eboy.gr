dd1bc7a921a0fb8b2d526fc75683fcf64976b41a
Giannisduke <admin@eboy.gr>
2025-09-18 11:49:43 +0300

    product meta

--- site/web/app/themes/simple-city/app/wc-template-hooks.php
+++ site/web/app/themes/simple-city/app/wc-template-hooks.php
@@ -151,7 +151,8 @@
 // remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10);
 // remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
 // remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
-// remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+ remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40);
+ add_action('woocommerce_single_product_summary', 'woocommerce_template_single_meta', 4);
 // remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_sharing', 50);
 
 /**
