aa213df19aec5d8e5c48584cca1cf98ef7183344
Giannisduke <admin@eboy.gr>
2025-12-24 14:56:53 +0200

    Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);

--- site/config/environments/staging.php
+++ site/config/environments/staging.php
@@ -26,7 +26,7 @@ Config::define('MULTISITE', true);
 Config::define('SUBDOMAIN_INSTALL', true); // Set to true if using subdomains
 Config::define('DOMAIN_CURRENT_SITE', 'eboy.gr');
 Config::define('PATH_CURRENT_SITE', '/');
-//Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
+Config::define('SITE_ID_CURRENT_SITE', env('SITE_ID_CURRENT_SITE') ?: 1);
 //Config::define('BLOG_ID_CURRENT_SITE', env('BLOG_ID_CURRENT_SITE') ?: 1);
 
 /**
