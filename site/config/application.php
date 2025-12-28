c79190e3cc2cd378bac2d10100949c4bfc3746bd
Giannisduke <admin@eboy.gr>
2025-12-28 15:51:55 +0200

    env('DOMAIN_CURRENT_SITE')

--- site/config/application.php
+++ site/config/application.php
@@ -150,7 +150,7 @@ ini_set('display_errors', '0');
 Config::define('WP_ALLOW_MULTISITE', true);
 Config::define('MULTISITE', true);
 Config::define('SUBDOMAIN_INSTALL', true); // Set to true if using subdomains
-Config::define('DOMAIN_CURRENT_SITE', 'eboy.gr');
+Config::define('DOMAIN_CURRENT_SITE', env('DOMAIN_CURRENT_SITE'));
 Config::define('PATH_CURRENT_SITE', '/');
 Config::define('SITE_ID_CURRENT_SITE', 1);
 Config::define('BLOG_ID_CURRENT_SITE', 1);
