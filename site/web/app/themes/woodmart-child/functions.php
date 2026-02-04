e2fe2f30d8631c854331adeae3cf5cccb1ce7ace
Giannisduke <admin@eboy.gr>
2026-02-04 17:05:35 +0200

    monk winter - css

--- site/web/app/themes/woodmart-child/functions.php
+++ site/web/app/themes/woodmart-child/functions.php
@@ -77,7 +77,7 @@ function antzel_custom_style2($hook) {
 
 function eboy_gsap_home() {
   if ( is_page( 41450 )  ) {
-    wp_enqueue_style( 'autumn_25_11', get_stylesheet_directory_uri().'/css/autumn_25_11.css' ); 
+    wp_enqueue_style( 'autumn_25_11', get_stylesheet_directory_uri().'/css/winter_26.css' ); 
     wp_enqueue_script( 'gsap_js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js', array(), false, true );
     wp_enqueue_script( 'gsap_draggables', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/Draggable.min.js', array(), false, true );
      wp_enqueue_script('embla-carousel', 'https://unpkg.com/embla-carousel/embla-carousel.umd.js', [], null, true);
@@ -92,8 +92,8 @@ add_action('wp_enqueue_scripts', 'eboy_gsap_home', 99999999999999991);
 
 
 function eboy_gsap_test() {
-  if ( is_page( 7075 ) || is_page( 51964 ) ) {
-    wp_enqueue_style( 'autumn_25_11', get_stylesheet_directory_uri().'/css/autumn_25_11.css' ); 
+  if ( is_page( 41450 ) || is_page( 51964 ) ) {
+    wp_enqueue_style( 'autumn_25_11', get_stylesheet_directory_uri().'/css/winter_26.css' ); 
     wp_enqueue_script( 'gsap_js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.6.1/gsap.min.js', array(), false, true );
     wp_enqueue_script( 'gsap_draggables', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.2.6/Draggable.min.js', array(), false, true );
      wp_enqueue_script('embla-carousel', 'https://unpkg.com/embla-carousel/embla-carousel.umd.js', [], null, true);
