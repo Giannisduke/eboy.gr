15fcfc938ecf9974ecc9a21766a03ea56118f152
Giannisduke <admin@eboy.gr>
2026-04-27 16:03:57 +0300

    simple-city - featured slider arrows

--- site/web/app/themes/simple-city/resources/views/partials/hero-embla.blade.php
+++ site/web/app/themes/simple-city/resources/views/partials/hero-embla.blade.php
@@ -1,4 +1,12 @@
 <section class="featured">
+
+<button class="embla__button embla__button--prev" type="button">
+    <svg class="embla__button__svg" viewBox="0 0 532 532"><path d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.795-36.208 13.795-50.001 0L118.34 291.413c-13.785-13.805-13.785-36.239 0-50.044l237.32-229.915z"/></svg>
+</button>
+<button class="embla__button embla__button--next" type="button">
+    <svg class="embla__button__svg" viewBox="0 0 532 532"><path d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.795 36.208-13.795 50.001 0L413.66 240.588c13.785 13.805 13.785 36.239 0 50.044L176.34 520.646z"/></svg>
+</button>
+
 <?php
 global $post;
 $page_content = $post ? $post->post_content : '';
@@ -55,14 +63,6 @@ wp_reset_postdata();
     </div>
 
     <div class="embla__controls">
-        <div class="embla__buttons">
-            <button class="embla__button embla__button--prev" type="button">
-                <svg class="embla__button__svg" viewBox="0 0 532 532"><path d="M355.66 11.354c13.793-13.805 36.208-13.805 50.001 0 13.785 13.804 13.785 36.238 0 50.034L201.22 266l204.442 204.61c13.785 13.805 13.785 36.239 0 50.044-13.793 13.795-36.208 13.795-50.001 0L118.34 291.413c-13.785-13.805-13.785-36.239 0-50.044l237.32-229.915z"/></svg>
-            </button>
-            <button class="embla__button embla__button--next" type="button">
-                <svg class="embla__button__svg" viewBox="0 0 532 532"><path d="M176.34 520.646c-13.793 13.805-36.208 13.805-50.001 0-13.785-13.804-13.785-36.238 0-50.034L330.78 266 126.34 61.391c-13.785-13.805-13.785-36.239 0-50.044 13.793-13.795 36.208-13.795 50.001 0L413.66 240.588c13.785 13.805 13.785 36.239 0 50.044L176.34 520.646z"/></svg>
-            </button>
-        </div>
         <div class="embla__dots"></div>
     </div>
 
